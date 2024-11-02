9818f79f34f697ad7791f41faec149f15de61d8d
duyhoangto <hoangtdse170030@fpt.edu.vn>
2024-11-02 19:16:03 +0700

    big model

---
+++ src/utilities/env.ts
@@ -0,0 +1,5 @@
+// export const Envs = {
+//     apiLocal: "https://swppetspaaaa-production.up.railway.app/",
+//     apiRemote: "https://swppetspaaaa-production.up.railway.app/",
+//     apiEnv: "https://swppetspaaaa-production.up.railway.app/",
+// };
